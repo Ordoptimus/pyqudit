bc12fe57ada9aea877c4f78a1a6b7604c23d98c3
Ordoptimus <entron@protonmail.com>
2021-07-12 13:58:02 +0530

    Bumped version to 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Aarya Bodhankar, Rutuja Andhale, Nikita Agrawal'
 author = 'Aarya Bodhankar'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
